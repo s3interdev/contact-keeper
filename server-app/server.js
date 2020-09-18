4710c40645411d5f60910726c05caff887479647
Michael Owuor <s3interdev@gmail.com>
2020-09-18 22:44:17 +0300

    Connecting MongoDB to the application.

--- server-app/server.js
+++ server-app/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const connectDb = require('./config/db');
 const morgan = require('morgan');
 
 const app = express();
@@ -6,17 +7,21 @@ const app = express();
 // middleware
 app.use(morgan('dev'));
 
+// connect to database
+connectDb();
+
 // initialize server port
 const PORT = process.env.PORT || 5000;
 
-// initialize server
+// initialize the server
 app.listen(PORT, () => console.log(`Express server started on port ${PORT}...`));
 
-// routes
+// default route
 app.get('/', (req, res) => {
 	res.json({ msg: 'Hello, welcome to the Contact Keeper API...' });
 });
 
+// define routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
