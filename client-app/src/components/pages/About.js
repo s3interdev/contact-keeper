e72ee00290492e9d6bcd2a4dfc1b8e52d8c2f6cc
Michael Owuor <s3interdev@gmail.com>
2020-09-20 17:44:32 +0300

    Cleaning up the react up, creating the Navbar and Router set up.

---
+++ client-app/src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+	return (
+		<div>
+			<h1>About Contact Keeper</h1>
+			<p className='my-1'>Full stack MERN application for managing contacts.</p>
+			<p className='bg-dark p'>
+				<strong>Version: </strong>1.0.0
+			</p>
+		</div>
+	);
+};
+
+export default About;
