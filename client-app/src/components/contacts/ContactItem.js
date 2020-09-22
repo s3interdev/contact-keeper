1db59a66594c936a70bbe1650403640f68059860
Michael Owuor <s3interdev@gmail.com>
2020-09-22 13:43:07 +0300

    Creating the delete contacts logic.

--- client-app/src/components/contacts/ContactItem.js
+++ client-app/src/components/contacts/ContactItem.js
@@ -6,10 +6,10 @@ const ContactItem = ({ contact }) => {
 	const contactContext = useContext(ContactContext);
 	const { deleteContact, setCurrentContact, clearCurrentContact } = contactContext;
 
-	const { id, name, email, phone, type } = contact;
+	const { _id, name, email, phone, type } = contact;
 
 	const onDelete = () => {
-		deleteContact(id);
+		deleteContact(_id);
 		clearCurrentContact();
 	};
 
--- client-app/src/context/contact/ContactState.js
+++ client-app/src/context/contact/ContactState.js
@@ -69,7 +69,7 @@ const ContactState = (props) => {
 	// delete contact
 	const deleteContact = async (id) => {
 		try {
-			await axios.delete(`/api/contacts${id}`);
+			await axios.delete(`/api/contacts/${id}`);
 
 			dispatch({
 				type: DELETE_CONTACT,
@@ -84,8 +84,25 @@ const ContactState = (props) => {
 	};
 
 	// update contact
-	const updateContact = (contact) => {
-		dispatch({ type: UPDATE_CONTACT, payload: contact });
+	const updateContact = async (contact) => {
+		const config = {
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		};
+
+		try {
+			const res = await axios.put(`/api/contacts/${contact._id}`, contact, config);
+			dispatch({
+				type: UPDATE_CONTACT,
+				payload: res.data,
+			});
+		} catch (err) {
+			dispatch({
+				type: CONTACT_ERROR,
+				payload: err.response.msg,
+			});
+		}
 	};
 
 	// clear contacts
